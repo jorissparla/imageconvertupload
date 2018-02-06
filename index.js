c49ab9359a8368e504c5660457c12a7dce43bbcf
joris sparla <joris.sparla@gmail.com>
2018-02-06 13:40:06 +0100

    fix

--- index.js
+++ index.js
@@ -5,19 +5,22 @@ const fs = require('fs');
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.get('/', (req, res) => {
   console.log('daar');
   res.send('Hallo');
 });
 /* updated */
-app.post('/', (req, res) => {
+app.post('/upload', (req, res) => {
   const base64String = req.body.image;
-  const name = req.body.name;
+
+  console.log(base64String.split('').length);
+  const name = 'c:\\Temp\\' + req.body.name;
   let base64Image = base64String.split(';base64,').pop();
   fs.writeFile(`${name}.png`, base64Image, { encoding: 'base64' }, function(err) {
     console.log('File created');
+    console.log(`${name}.png`);
   });
   res.send('ok');
 });
